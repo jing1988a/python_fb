5461c97cae27bfca01fbaac8455d560b918c5b87
jing1988a <jing1988a@gmail.com>
2018-10-03 09:45:27 -0400

    740 738 754 748

---
+++ 900plus/MonotoneIncreasingDigits738.py
@@ -0,0 +1,24 @@
+# Given a non-negative integer N, find the largest number that is less than or equal to N with monotone increasing digits.
+#
+# (Recall that an integer has monotone increasing digits if and only if each pair of adjacent digits x and y satisfy x <= y.)
+#
+# Example 1:
+# Input: N = 10
+# Output: 9
+# Example 2:
+# Input: N = 1234
+# Output: 1234
+# Example 3:
+# Input: N = 332
+# Output: 299
+# Note: N is an integer in the range [0, 10^9].
+#
+# 1232    1222  1199
+
+class Solution:
+    def monotoneIncreasingDigits(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+
